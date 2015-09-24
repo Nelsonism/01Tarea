e5b4099df5f74f9c3eaabda86b4d52e547773175
Nelsonism <nelson_bkn_1993@hotmail.com>
2015-09-24 19:00:25 -0300

    actualizado el archivo, terminado hasta la parte 2

--- tarea1.py
+++ tarea1.py
@@ -5,10 +5,19 @@ import matplotlib
 b=numpy.loadtxt('sun_AM0.dat')
 LongitudOnda=[]
 Flujo=[]
+integral=0
 for par in b:
     a=np.array(par)
     LongitudOnda.append(a[0])
     Flujo.append(a[1])
+#for a in range(1695):
+#    LongDif=LongitudOnda[a+1]-LongitudOnda[a]
+#    PromFlujo=(Flujo[a]+Flujo[a+1])/2
+#    Add=PromFlujo*LongDif
+#    integral=integral+Add
+#print (integral)
+J=np.trapz(Flujo,LongitudOnda)
+print (J)
 plot(LongitudOnda,Flujo)
 xlabel('Longitud de onda (nm)')
 ylabel('Flujo (W/m^2/nm)')
